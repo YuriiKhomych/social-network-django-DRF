11df0decc518d40c2fb5871e26118b4aa9b3397b
yuriikhomych <yuriykhomich@gmail.com>
2018-09-25 22:37:35 +0300

    Remove unneccesary network app

--- social_network/network/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CreateUserAPIView, LoginUserAPIView, RetrieveUpdateUserAPI
-
-urlpatterns = [
-    path('user/create/', CreateUserAPIView.as_view()),
-    path('user/get_token/', LoginUserAPIView.as_view()),
-    path('user/update/', RetrieveUpdateUserAPI.as_view()),
-]
