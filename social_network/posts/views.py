afd2bfbc762fdd125104c4010280d0d51fde428b
yuriikhomych <yuriykhomich@gmail.com>
2018-09-25 22:40:21 +0300

    Create AllPostAPIView and PostCreateAPIView for posts app

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from .models import Post
+from .serializers import PostCreateSerializer, AllPostSerializer
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class AllPostAPIView(ListAPIView):
+    """
+    This view will return response with full articles
+    (title, body, data, author, liked_by),
+    but divided into 10 users objects at the one response.
+    Used limit and offset.
+    """
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = AllPostSerializer
+    pagination_class = LimitOffsetPagination
+
+
+class PostCreateAPIView(ListCreateAPIView):
+    """
+    This view will check input user unique title name and then,
+    if data is valid, create new article
+    and return success message
+    """
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
+
+    def perform_create(self, serializer):
+        # The request user is set as author automatically.
+        serializer.save(author=self.request.user)
