90fba8dd759aac9ea4928515008ea1cafb2a640a
yuriikhomych <yuriykhomich@gmail.com>
2018-09-27 10:10:45 +0300

    Add path for like post functionality

--- social_network/posts/urls.py
+++ social_network/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostCreateAPIView, AllPostAPIView, PostRetrieveUpdateDestroyAPIView
+from .views import PostCreateAPIView, AllPostAPIView, \
+    PostRetrieveUpdateDestroyAPIView, PostLikeAPIView
 
 urlpatterns = [
     path('', AllPostAPIView.as_view()),
     path('create/', PostCreateAPIView.as_view()),
     path('<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('like/<int:post_id>/', PostLikeAPIView.as_view()),
 ]
