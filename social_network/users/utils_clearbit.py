8eac2fba82e58cd00e7a24ecf1fe57f06a570092
yuriikhomych <yuriykhomich@gmail.com>
2018-09-28 09:24:14 +0300

    Add utils_clearbit.py with get_data function functionality

---
+++ social_network/users/utils_clearbit.py
@@ -0,0 +1,15 @@
+import clearbit
+
+clearbit.key = 'KEY'
+
+
+def get_data(email):
+    response = clearbit.Enrichment.find(email=email, stream=True)
+    data = {}
+    if response:
+        if response['person'] is not None:
+            data['first_name'] = response['person']['name']['givenName']
+            data['last_name'] = response['person']['name']['familyName']
+        if response['company'] is not None:
+            data['company'] = response['company']['name']
+    return data
