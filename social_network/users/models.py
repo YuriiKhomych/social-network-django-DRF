1d591465cd557c4fdc92f51293708d1b99249fcb
yuriikhomych <yuriykhomich@gmail.com>
2018-09-28 09:21:09 +0300

    Add company field to User model

--- social_network/users/models.py
+++ social_network/users/models.py
@@ -37,6 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=40, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
+    company = models.CharField(max_length=50, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
