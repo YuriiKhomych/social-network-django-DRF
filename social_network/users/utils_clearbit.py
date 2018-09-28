3153de132b955561a0e6838bc0b4b4b2517f4060
yuriikhomych <yuriykhomich@gmail.com>
2018-09-28 21:05:19 +0300

    Add try except to get data clearbit function

--- social_network/users/utils_clearbit.py
+++ social_network/users/utils_clearbit.py
@@ -4,12 +4,15 @@ clearbit.key = 'KEY'
 
 
 def get_data(email):
-    response = clearbit.Enrichment.find(email=email, stream=True)
-    data = {}
-    if response:
-        if response['person'] is not None:
-            data['first_name'] = response['person']['name']['givenName']
-            data['last_name'] = response['person']['name']['familyName']
-        if response['company'] is not None:
-            data['company'] = response['company']['name']
-    return data
+    try:
+        response = clearbit.Enrichment.find(email=email, stream=True)
+        data = {}
+        if response:
+            if response['person'] is not None:
+                data['first_name'] = response['person']['name']['givenName']
+                data['last_name'] = response['person']['name']['familyName']
+            if response['company'] is not None:
+                data['company'] = response['company']['name']
+        return data
+    except:
+        return
