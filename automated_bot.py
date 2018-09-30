64a7963b4c7985cff34f35b79b199f4e3ac7cc45
yuriikhomych <yuriykhomich@gmail.com>
2018-09-30 23:12:10 +0300

    Add get_random_domain and get_random_name functions

--- automated_bot.py
+++ automated_bot.py
@@ -1,4 +1,10 @@
+import random
+
+from string import ascii_lowercase
 from configparser import ConfigParser
+
+domains = ["hotmail.com", "gmail.com", "i.ua", "mail.com", "yahoo.com"]
+letters = ascii_lowercase
 
 
 def get_config_data(path='bot_settings.ini'):
@@ -9,3 +15,11 @@ def get_config_data(path='bot_settings.ini'):
         'max_posts_per_user': config.get("Settings", "max_posts_per_user"),
         'max_likes_per_user': config.get("Settings", "max_likes_per_user"),
     }
+
+
+def get_random_domain(domains):
+    return random.choice(domains)
+
+
+def get_random_name(letters, length):
+    return ''.join(random.choice(letters) for _ in range(length))
